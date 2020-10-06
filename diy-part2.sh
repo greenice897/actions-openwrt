d953d4aafde88cc0fbb3853320b7fe58a897c1ed
greenice897 <greenice897@gmail.com>
2020-10-06 14:53:32 +0800

    compile on 20201006

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@ sed -i "/^DISTRIB_REVISION=/cDISTRIB_REVISION='V$YEAR.$MONTH.$DAY By Greenice'" 
 # sed -i '/REDIRECT --to-ports 53/d' package/network/config/firewall/files/firewall.user
 # echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> package/network/config/firewall/files/firewall.user
 # echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> package/network/config/firewall/files/firewall.user
+
+##golang源改为lean修复官方源码下v2ray 4.30.0编译错误
+rm -rf feeds/packages/lang/golang
+svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
