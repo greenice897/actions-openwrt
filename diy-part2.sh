2b8f04e262248ff9143f381936eaf95e971a1778
greenice897 <greenice897@gmail.com>
2022-04-21 13:52:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,9 +4,9 @@ YEAR=$(date +%y)
 MONTH=$(date +%-m)
 DAY=$(date +%-d)
 
-mv ./package/greenice/ucl ./tools/
-mv ./package/greenice/upx ./tools/
-sed -i '28 a\tools-y += ucl upx' ./tools/Makefile
+mv package/greenice/ucl tools/
+mv package/greenice/upx tools/
+sed -i '28 a\tools-y += ucl upx' tools/Makefile
 
 ##修改LAN地址、时区
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
@@ -16,9 +16,9 @@ sed -i "s/timezone='UTC'/timezone='CTS-8'/g" package/base-files/files/bin/config
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 sed -i "/^DISTRIB_REVISION=/cDISTRIB_REVISION='V$YEAR.$MONTH.$DAY By Greenice'" package/base-files/files/etc/openwrt_release
 
-# rm -rf ./feeds/packages/net/adguardhome
-rm -rf ./feeds/packages/net/xray-core
-# mv ./package/greenice/adguardhome ./feeds/packages/net/
-mv ./package/greenice/xray-core ./feeds/packages/net/
+# rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/xray-core
+# mv package/greenice/adguardhome feeds/packages/net/
+mv package/greenice/xray-core feeds/packages/net/
 
 exit 0
