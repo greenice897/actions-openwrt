a9454af33104bb700ff293457d0d2a17c8ff2c9a
greenice897 <greenice897@gmail.com>
2024-08-07 06:42:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,15 +16,34 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 ########### 设置密码为空（可选） ###########
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
+# rm -rf package/greenice/passwallpackages/gn
+# rm -rf package/greenice/passwallpackages/naiveproxy
+rm -rf package/network/services/omcproxy
+rsync -rtv --delete package/greenice/omcproxy/ package/network/services/omcproxy
+rm package/greenice/omcproxy
+# svn export https://github.com/greenice897/gipackages/trunk/omcproxy package/network/services/omcproxy
+rm -rf package/greenice/passwallpackages/sing-box
+rm -rf feeds/packages/net/sing-box
+rsync -rtv --delete package/greenice/sing-box/ feeds/packages/net/sing-box
+rsync -rtv --delete package/greenice/sing-box/  package/greenice/passwallpackages/sing-box
+# svn export https://github.com/greenice897/gipackages/trunk/sing-box feeds/packages/net/sing-box
+# svn export https://github.com/greenice897/gipackages/trunk/sing-box package/greenice/passwallpackages/sing-box
+
 ########### 更新smartdns版本 ###########
-#rm -rf feeds/packages/net/smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-#sed -i "/^PKG_VERSION:=/cPKG_VERSION:=1.2022.40" feeds/packages/net/smartdns/Makefile
-#sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=b473b9c6e101f89ec302c7e080fa7f6bd59ca618" feeds/packages/net/smartdns/Makefile
-#sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
-#sed -i "51i     \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" feeds/packages/net/smartdns/Makefile
+rm -rf feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+sed -i "s/PKG_VERSION:=1.2024.45/PKG_VERSION:=1.2024.46/" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=75649b623519dda99b772342bbbe625a45234864" feeds/packages/net/smartdns/Makefile
+sed -i "s/PKG_MIRROR_HASH/# PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+sed -i "s/PKG_VERSION:=1.2024.45/PKG_VERSION:=1.2024.46/" feeds/luci/applications/luci-app-smartdns/Makefile
 ########### 更新adguardhome版本###########
 #rsync -rtv --delete package/greenice/adguardhome/ feeds/packages/net/adguardhome
 
 ########### 更新xray-core版本###########
-#rsync -rtv --delete package/greenice/xray-core/ feeds/packages/net/xray-core
+rsync -rtv --delete package/greenice/passwallpackages/xray-core/ feeds/packages/net/xray-core
+rsync -rtv --delete package/greenice/passwallpackages/microsocks/ feeds/packages/net/microsocks
+# rsync -rtv --delete package/greenice/passwallpackages/sing-box/ feeds/packages/net/sing-box
+rsync -rtv --delete package/greenice/passwallpackages/v2ray-core/ feeds/packages/net/v2ray-core
+rsync -rtv --delete package/greenice/passwallpackages/v2ray-geodata/ feeds/packages/net/v2ray-geodata
