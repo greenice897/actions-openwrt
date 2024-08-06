4023bf9815b6f6b24eeaee98c372470c2b6d592b
greenice897 <greenice897@gmail.com>
2024-08-07 06:40:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -19,13 +19,6 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-
-svn co https://github.com/greenice897/gipackages/trunk/r8125 package/kernel/r8125
-####### 科学上网插件 #######
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
-#echo 'src-git openclash_luci https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-
-
-####### darkmatter主题 #######
-# echo 'src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' >>feeds.conf.default
+git clone https://github.com/greenice897/openwrtpackages.git package/greenice
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/greenice/passwallpackages
+git clone https://github.com/sbwml/package_kernel_r8125.git package/greenice/r8125
