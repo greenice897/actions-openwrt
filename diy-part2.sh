794a79fa342f48fda63196201058afc8acfe9b61
greenice897 <greenice897@gmail.com>
2024-09-15 13:48:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,7 +38,7 @@ rsync -rtv --delete package/greenice/passwallpackages/sing-box/ feeds/packages/n
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 sed -i "s/PKG_VERSION:=1.2024.45/PKG_VERSION:=1.2024.46/" feeds/packages/net/smartdns/Makefile
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=d248f6d1408d22ea8920f7036572ba3b6ae93674" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=d748b9ed91d13ebdd37910de9e233b7a7d3b58f5" feeds/packages/net/smartdns/Makefile
 sed -i "s/PKG_MIRROR_HASH/# PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
